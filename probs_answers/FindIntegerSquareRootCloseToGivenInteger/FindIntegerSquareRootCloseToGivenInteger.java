80096bc31bee706f7f86e3c928d067f06dca3c34
huseyinbarin <developer.barin@gmail.com>
2017-05-22 11:53:16 +0300

    math function added

--- probs_answers/FindIntegerSquareRootCloseToGivenInteger/FindIntegerSquareRootCloseToGivenInteger.java
+++ probs_answers/FindIntegerSquareRootCloseToGivenInteger/FindIntegerSquareRootCloseToGivenInteger.java
@@ -11,9 +11,9 @@ class FindIntegerSquareRootCloseToGivenInteger {
             throw new Exception("k cannot be lower than zero.");
         }
 
-
         long l = 0;
         long r = k;
+
 
         while (l <= r) {
             long mid = l + ((r - l) / 2);
@@ -25,5 +25,20 @@ class FindIntegerSquareRootCloseToGivenInteger {
             }
         }
         return (int) l - 1;
+
+    }
+
+
+    //alternative solution... if allowed...
+    static int findIntegerSquareRootCloseToGivenInteger2(int k) throws Exception {
+
+        if (k < 0) {
+            throw new Exception("k cannot be lower than zero.");
+        }
+
+        double res = Math.exp(0.5 * Math.log(Double.parseDouble("" + k)));
+
+
+        return (int) res;
     }
 }
--- probs_answers/FindIntegerSquareRootCloseToGivenInteger/FindIntegerSquareRootCloseToGivenIntegerTest.java
+++ probs_answers/FindIntegerSquareRootCloseToGivenInteger/FindIntegerSquareRootCloseToGivenIntegerTest.java
@@ -25,16 +25,28 @@ public class FindIntegerSquareRootCloseToGivenIntegerTest {
 
     @Test
     public void findSquareOfInteger() throws Exception {
-        int[] k = {1,300, 17, 26, 37, 100};
-        int[] expected = {1,17, 4, 5, 6, 10};
+        int[] k = {1, 300, 17, 26, 37, 100};
+        int[] expected = {1, 17, 4, 5, 6, 10};
         int[] actual = new int[k.length];
 
         for (int i = 0; i < k.length; i++) {
             actual[i] = FindIntegerSquareRootCloseToGivenInteger.findIntegerSquareRootCloseToGivenInteger(k[i]);
-            System.out.println(k[i]+ " => " + expected[i]  + " =" + actual[i]);
+            System.out.println(k[i] + " => " + expected[i] + " =" + actual[i]);
             assertTrue(expected[i] == actual[i]);
         }
     }
 
+    @Test
+    public void findSquareOfInteger2() throws Exception {
+        int[] k = {1, 300, 17, 26, 37, 100};
+        int[] expected = {1, 17, 4, 5, 6, 10};
+        int[] actual = new int[k.length];
+
+        for (int i = 0; i < k.length; i++) {
+            actual[i] = FindIntegerSquareRootCloseToGivenInteger.findIntegerSquareRootCloseToGivenInteger2(k[i]);
+            System.out.println(k[i] + " => " + expected[i] + " =" + actual[i]);
+            assertTrue(expected[i] == actual[i]);
+        }
+    }
 
 }
